d0ce8239da282c1178dd04fa5bc1d48b44529950
ryunosuke1204 <boxing.ryu1204@gmail.com>
2022-06-09 08:44:17 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :email, presence: true
+  validates :password_confirmation, presence: true
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   
 end
