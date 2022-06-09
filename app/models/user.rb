d3efd991244be9ec62f957129f147a2cb44d92f2
ryunosuke1204 <boxing.ryu1204@gmail.com>
2022-06-09 09:08:30 +0900

    新規登録機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, presence: true
-  validates :password_confirmation, presence: true
   validates :nickname, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   
+has_many :ppsts
+has_many :comments
 end
