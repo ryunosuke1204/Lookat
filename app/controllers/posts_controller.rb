e79632bf2e822512be164424b792704eaff5a46e
ryunosuke1204 <boxing.ryu1204@gmail.com>
2022-06-10 11:56:04 +0900

    コントローラーの修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!,except: [:index,:show]
   before_action :set_post, only: [:show, :edit,:update,:destroy]
+  before_action :move_to_index, except: [:index, :show ,:new, :create]
 
   def index
     @posts = Post.all
@@ -41,6 +43,12 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:headwear,:tops,:shoes,:accessories,:bottoms,:outer,:image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless current_user.id == @post.user_id 
+      redirect_to action: :index
+    end
   end
 
   def set_post
