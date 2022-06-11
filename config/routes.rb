8f0f26919992bae92b1e38eed95191e4ea3a3caf
ryunosuke1204 <boxing.ryu1204@gmail.com>
2022-06-11 14:54:55 +0900

    フォロー機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
   root to: 'posts#index'
   resources :posts do
@@ -7,5 +9,8 @@ Rails.application.routes.draw do
   end
   resources :users, only: :show do
     get :favorites, on: :collection
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
  end
 end
