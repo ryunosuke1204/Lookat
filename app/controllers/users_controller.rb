0c37f1ce2b9cf597e9dd5fb4aef07e0fd8522ae9
ryunosuke1204 <boxing.ryu1204@gmail.com>
2022-06-14 12:36:03 +0900

    ログインと新規登録のビュー変更　ユーザーカラムの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   def show
   @user = User.find(params[:id])
-  @name = user.name
-  @posts = @user.post
+  @name = @user.name
+  @posts = @user.posts
 
   favorites = Favorite.where(user_id: current_user.id).pluck(:post_id) 
   @favorite_list = Post.find(favorites)
--- db/schema.rb
+++ db/schema.rb
@@ -78,6 +78,8 @@ ActiveRecord::Schema.define(version: 2022_06_11_054611) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name", null: false
+    t.text "image"
+    t.text "content"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
