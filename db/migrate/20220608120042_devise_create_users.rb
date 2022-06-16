2328e6aa82901596143c1f1736aba534d472996e
ryunosuke1204 <boxing.ryu1204@gmail.com>
2022-06-16 09:29:40 +0900

    マイページ実装

--- db/migrate/20220608120042_devise_create_users.rb
+++ db/migrate/20220608120042_devise_create_users.rb
@@ -7,7 +7,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :name, null: false
-      t.text :image
       t.text :content
 
       ## Recoverable
