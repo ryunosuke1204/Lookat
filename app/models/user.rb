9de352daa6fd72396e9c96d3b111f8437ddb9c2a
ryunosuke1204 <boxing.ryu1204@gmail.com>
2022-06-21 12:31:25 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 validates :name, presence: true
+validates :profile, presence: true
 has_many :posts
 has_many :favorites
 
